9981a8a342688c2ce14cff9d7cdfc85a47d2f517
mimaolu <1123605771@qq.com>
2020-04-12 20:18:47 +0800

    cli 添加 father 模块打包工具

---
+++ .fatherrc.js
@@ -0,0 +1,12 @@
+export default {
+    esm: 'babel',
+    cjs: 'babel',
+    autoprefixer: {
+        browsers: [
+          'ie>8',
+          'Safari >= 6',
+        ],
+    },
+    disableTypeCheck: true,
+
+  }
