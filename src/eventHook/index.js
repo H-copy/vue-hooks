87cec91a00d57fcbdbbd60f638205bf8b2e5813a
胡海国 <huhaiguo@zkdntech.com>
2020-04-17 11:32:31 +0800

    refactor

--- src/eventHook/index.js
+++ src/eventHook/index.js
@@ -1,3 +1,5 @@
+import { ref, watch, computed } from '@vue/composition-api'
+
 
 /**
  * dom 事件绑定
@@ -5,21 +7,39 @@
  * @param { Function } handler 回调函数
  * @param { Element } target 绑定对象
  * @param { Object } option 配置属性
- * @returns { Function } remove 事件移除函数
+ * @returns { [ remove, element, reBind ] } remove 事件移除函数 element 绑定元素 reBind 重新绑定
  */
-export function eleEventHook( name, handler, target, option ){
+export function eleEventHook( name, handler, target ){
 
+    const element = ref(null)
+    let remove = ref(null)
+    let reBind = ref(null)
+    
+    const targetElement = computed(() => {
+        return target&&target.value ? target.value : element.value
+    })
+    
     if(!handler) return
-    if(!target) return
+    
 
-    const addEvent = target.addEventListener || target.on
-    const removeEvent = target.removeEventListener || target.off
-    const remove = () => removeEvent.call(target, name, handler, option)
-    addEvent.call(target, name, handler, Option)
+    watch(() => targetElement.value, (val, oldVal) =>{
 
-    return remove
+        if(oldVal){
+          remove.value && remove.value()
+        }
+
+        if(val){
+          val.addEventListener(name, handler)
+          reBind.value = () => val.addEventListener(name, handler)
+          remove.value = () => val.removeEventListener(name, handler)
+        }
+
+    })
+    
+    return [ remove, element, reBind ]
 }
- 
+
+
 /**
  * vm 事件绑定
  * @param { String } name 事件名称 
@@ -34,12 +54,12 @@ export function eleEventHook( name, handler, target, option ){
  * const removeTime = vmEventHook('time', () => console.log(new Date()), vm)
  *  
  */
-export function vmEventHook (name, handler, vm, option){
+export function vmEventHook (name, handler, vm ){
 
     if(!handler)return
     if(!vm) return
 
-    vm.$on(name, handler, option)
+    vm.$on(name, handler)
     const remove = () => vm.$off(name)
     return remove
 
